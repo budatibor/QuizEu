9e81d49960c7e77abc200cb0bb54b0c1d3447705
Buda Tibor <tibordaniel1997@gmail.com>
2019-05-15 00:09:05 +0200

    Now it can generate .jar

---
+++ src/main/java/controller/Main.java
@@ -0,0 +1,12 @@
+package controller;
+
+public class Main {
+    /**
+     * The main method of the main class.
+     *
+     * @param args arguments the user wants to pass.
+     */
+    public static void main(String[] args) {
+        QuizMain.main(args);
+    }
+}
